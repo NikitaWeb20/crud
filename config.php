7233fda82434dbba967fd452aff8018f7645e88c
Your Name <you@example.com>
2020-05-17 22:09:42 +0300

    3.0 Update

--- config.php
+++ config.php
@@ -4,6 +4,4 @@ try {
   $pdo = new PDO('mysql:dbname=Database; host=localhost', 'root', 'root');
 } catch (PDOException $e) {
   die($e->getMessage());
-}
-
-?>
+}?>
--- edit.php
+++ edit.php
@@ -1,4 +1,36 @@
 <?php
     require('config.php');
+    $sql = "SELECT * FROM `users` WHERE `id` = ?";
+    $sql = $pdo->prepare($sql);
+    $sql->execute([$_GET['edit']]);
+    $result = $sql->fetchAll();
+    if (isset($_POST["submit_update"])) {
+        $sql = "UPDATE `users` SET `name`= ?,`description`= ?,`sum`= ? WHERE `id` = ? ";
+        $sql = $pdo->prepare($sql);
+        $sql -> execute([$_POST['new_name'],$_POST['new_description'],$_POST['new_sum'],$_GET['edit']]);
+        header('Location: index.php');
+    }
     
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Редактирование</title>
+</head>
+<body>
+    <form action="#" method="post">
+    <?php foreach ($result as $value) { ?>
+        <p>Имя:</p>
+        <input type="text" value='<?=$value['name']?>' name="new_name">
+        <p>Описание:</p>
+        <input type="text" value='<?=$value['description']?>' name="new_description">
+        <p>Сумма:</p>
+        <input type="text" value='<?=$value['sum']?>' name="new_sum">
+        <input type="submit" name="submit_update">
+    <?php } ?>
+    </form>
+</body>
+</html>
