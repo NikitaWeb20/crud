de85c6700e32ed530b71c0b69b31465a41760e60
Your Name <you@example.com>
2020-05-17 20:55:39 +0300

    2.0. Delete

---
+++ delete.php
@@ -0,0 +1,26 @@
+<?php
+    require('config.php');
+
+    $id = $_GET['delete'];
+    if (isset($_POST['submit_delete'])) {
+        $sql = "DELETE FROM `users` WHERE `id`=?";
+        $query = $pdo->prepare($sql);
+        $query->execute([$id]);
+        header('Location: index.php');
+    }
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Удалить?</title>
+</head>
+<body>
+    <form action="#" method="POST">
+        <p>Вы действительно хотите удалить запись?</p>
+        <input type="submit" value="Удалить" name="submit_delete">
+    </form>
+</body>
+</html>
